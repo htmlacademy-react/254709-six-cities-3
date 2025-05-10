63f03a1fd4a41e1684b65e2d9a3fbb1c893491a7
Влад <v1ad@inbox.ru>
2025-05-10 17:19:49 +0300

    - Добавит хук с картами - Добавит интерактивную карту в проект - Добавит пакет classNames для более лаконичного вывода карт

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import { AppRoute } from '../../const';
 import { calculateRating } from '../../utils';
 import { OfferType } from '../../mocks/offers';
+import cn from 'classnames';
 
 type CardProps = {
   offer: OfferType;
@@ -9,7 +10,11 @@ type CardProps = {
   isFavoritePage?: boolean;
 };
 
-const Card = ({ offer, handleHover, isFavoritePage }: CardProps): JSX.Element => {
+const Card = ({
+  offer,
+  handleHover,
+  isFavoritePage,
+}: CardProps): JSX.Element => {
   const {
     isFavorite,
     id,
@@ -26,19 +31,29 @@ const Card = ({ offer, handleHover, isFavoritePage }: CardProps): JSX.Element =>
 
   return (
     <article
-      className={`${isFavoritePage ? 'favorites__card' : 'cities__card'} place-card`}
+      className={cn(
+        'place-card',
+        { 'favorites__card': isFavoritePage },
+        { 'cities__card': !isFavoritePage }
+      )}
       onMouseEnter={handleMouseOn}
       onMouseLeave={handleMouseOff}
     >
       {isPremium && (
-        <div className='place-card__mark'>
+        <div className="place-card__mark">
           <span>Premium</span>
         </div>
       )}
-      <div className={`${isFavoritePage ? 'favorites__image-wrapper' : 'cities__image-wrapper'} place-card__image-wrapper`}>
+      <div
+        className={cn(
+          'place-card__image-wrapper',
+          { 'favorites__image-wrapper': isFavoritePage },
+          { 'cities__image-wrapper': !isFavoritePage }
+        )}
+      >
         <Link to={`${AppRoute.Offer}/${id}`}>
           <img
-            className='place-card__image'
+            className="place-card__image"
             src={previewImage}
             width={isFavoritePage ? 150 : 260}
             height={isFavoritePage ? 110 : 200}
@@ -46,33 +61,41 @@ const Card = ({ offer, handleHover, isFavoritePage }: CardProps): JSX.Element =>
           />
         </Link>
       </div>
-      <div className={`${isFavoritePage && 'favorites__card-info'} place-card__info`}>
-        <div className='place-card__price-wrapper'>
-          <div className='place-card__price'>
-            <b className='place-card__price-value'>&euro;{price}</b>
-            <span className='place-card__price-text'>&#47;&nbsp;night</span>
+      <div
+        className={`${
+          isFavoritePage && 'favorites__card-info'
+        } place-card__info`}
+      >
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">&euro;{price}</b>
+            <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
           <button
-            className={`place-card__bookmark-button button ${isFavorite && 'place-card__bookmark-button--active'}`}
+            className={`place-card__bookmark-button button ${
+              isFavorite && 'place-card__bookmark-button--active'
+            }`}
           >
-            <svg className='place-card__bookmark-icon' width='18' height='19'>
-              <use xlinkHref='#icon-bookmark'></use>
+            <svg className="place-card__bookmark-icon" width="18" height="19">
+              <use xlinkHref="#icon-bookmark"></use>
             </svg>
-            <span className='visually-hidden'>
+            <span className="visually-hidden">
               {isFavorite ? 'In bookmarks' : 'To bookmarks'}
             </span>
           </button>
         </div>
-        <div className='place-card__rating rating'>
-          <div className='place-card__stars rating__stars'>
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
             <span style={{ width: `${calculateRating(rating)}` }} />
-            <span className='visually-hidden'>Rating</span>
+            <span className="visually-hidden">Rating</span>
           </div>
         </div>
-        <h2 className='place-card__name'>
-          <a href='#'>{title}</a>
+        <h2 className="place-card__name">
+          <a href="#">{title}</a>
         </h2>
-        <p className='place-card__type'>{type.charAt(0).toUpperCase() + type.slice(1)}</p>
+        <p className="place-card__type">
+          {type.charAt(0).toUpperCase() + type.slice(1)}
+        </p>
       </div>
     </article>
   );
